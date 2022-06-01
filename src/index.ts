c70f2c7db41fe218cf43b98c5a9b9cd5b262a473
hermanb <bideherman@yahoo.com>
2022-05-31 18:58:04 -0700

    updated all function methods

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,17 @@ import { IEmployeeData,IEmployeeObj } from './IEmployee';
 function main() {
     const companyTree = generateCompanyStructure(employeesData)
     console.dir(companyTree, {depth:null, color:true})
-    // const hire = hireEmployee(companyTree, "Jeb", "Sarah")
-    // const fire = fireEmployee(companyTree, "Alicia")
-    // const sub = promoteEmployee(companyTree, "Jared")
+    const hire = hireEmployee(companyTree, "Jeb", "Sarah")
+    const fire = fireEmployee(companyTree, "Alicia")
+    const sub = promoteEmployee(companyTree, "Jared")
     const demote = demoteEmployee(companyTree, "Xavier", "Maria")
+    getBoss(companyTree, "jared")
+    getSubordinates(companyTree, "Maria")
     console.dir(companyTree, {depth:null, color:true})
-    // promoteEmployee(companyTree,)
     // console.log(hire)
     // console.log(fire)
     // console.log(sub)
     // console.log(demote)
-    // getBoss(companyTree, "jared")
-    // getSubordinates(companyTree, "Maria")
     // console.dir(companyTree, {depth:null, color:true})
 }
 
